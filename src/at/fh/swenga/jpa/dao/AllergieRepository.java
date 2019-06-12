c1d75b6515b5c927bafcfbee6885569721936ffd
julian <Delazer_Julian@hotmail.de>
2019-06-12 13:22:04 +0200

    Add Repositories

---
+++ src/at/fh/swenga/jpa/dao/AllergieRepository.java
@@ -0,0 +1,20 @@
+package at.fh.swenga.jpa.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import at.fh.swenga.jpa.model.AllergieModel;
+
+@Repository
+@Transactional
+public interface AllergieRepository extends JpaRepository<AllergieModel, Integer> {
+	
+ 
+
+}
