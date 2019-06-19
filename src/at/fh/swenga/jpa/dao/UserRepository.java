b40ef43485e13fe35335f8b0b46ca4d8e9748543
Urb4nOutl4w <tim.braunauer@edu.fh-joanneum.at>
2019-06-20 01:49:14 +0200

    minor bugfixes in userPreferences

--- src/at/fh/swenga/jpa/dao/UserRepository.java
+++ src/at/fh/swenga/jpa/dao/UserRepository.java
@@ -22,28 +22,28 @@ public interface UserRepository extends JpaRepository<UserModel, Integer> {
 	// give me the user that has reported that recipe
 	@Query ("SELECT u "
 			+ "FROM UserModel AS u "
-			+ "JOIN u.reportedRecipes r "
+			+ "LEFT JOIN u.reportedRecipes r "
 			+ "WHERE r.id = :recId AND u.id = :userId")
 	public UserModel findReportingUserFromRecipe(@Param("recId") int recId, @Param("userId") int userId);
 	
 	// give me the user that has reported that recipe
 	@Query ("SELECT u "
 			+ "FROM UserModel AS u "
-			+ "JOIN u.likedRecipes r "
+			+ "LEFT JOIN u.likedRecipes r "
 			+ "WHERE r.id = :recId AND u.id = :userId")
 	public UserModel findLikingUserFromRecipe(@Param("recId") int recId, @Param("userId") int userId);
 	
 	// give me the user with the username with its reportedRecipes
 	@Query ("SELECT u "
 			+ "FROM UserModel AS u "
-			+ "JOIN FETCH u.reportedRecipes r "
+			+ "LEFT JOIN FETCH u.reportedRecipes r "
 			+ "WHERE LOWER(u.userName) = LOWER(:searchString) ")
 	public UserModel findUserByUserNameWithReportedRecipes(@Param("searchString") String searchString);
 		
 	// give me the user with the username with its likedRecipes
 	@Query ("SELECT u "
 			+ "FROM UserModel AS u "
-			+ "JOIN FETCH u.likedRecipes r "
+			+ "LEFT JOIN FETCH u.likedRecipes r "
 			+ "WHERE LOWER(u.userName) = LOWER(:searchString) ")
 	public UserModel findUserByUserNameWithLikedRecipes(@Param("searchString") String searchString);
 	
@@ -51,60 +51,60 @@ public interface UserRepository extends JpaRepository<UserModel, Integer> {
 	// give me the user with the id with its recipes and likedRecipes
 	@Query ("SELECT u "
 			+ "FROM UserModel AS u "
-			+ "JOIN FETCH u.likedRecipes lr "
-			+ "JOIN FETCH u.recipes r "
+			+ "LEFT JOIN FETCH u.likedRecipes lr "
+			+ "LEFT JOIN FETCH u.recipes r "
 			+ "WHERE u.id = :userId ")
 	public UserModel findUserByIdWithRecipesAndLikedRecipes(@Param("userId") int userId);
 		
 	// give me the user with the username with its recipes and likedRecipes
 	@Query ("SELECT u "
 			+ "FROM UserModel AS u "
-			+ "JOIN FETCH u.likedRecipes lr "
-			+ "JOIN FETCH u.recipes r "
+			+ "LEFT JOIN FETCH u.likedRecipes lr "
+			+ "LEFT JOIN FETCH u.recipes r "
 			+ "WHERE LOWER(u.userName) = LOWER(:userName) ")
 	public UserModel findUserByIdWithRecipesAndLikedRecipes(@Param("userName") String userName);
 	
 	// give me the user with the username with its recipes and likedRecipes
 	@Query ("SELECT u "
 			+ "FROM UserModel AS u "
-			+ "JOIN FETCH u.usersIFollow uif "
+			+ "LEFT JOIN FETCH u.usersIFollow uif "
 			+ "WHERE LOWER(u.userName) = LOWER(:userName) ")
 	public UserModel findUserByUserNameWithUsersIFollow(@Param("userName") String userName);
 	
 	// give me the user that has reported that recipe
 	@Query ("SELECT u "
 			+ "FROM UserModel AS u "
-			+ "JOIN u.usersIFollow uif "
+			+ "LEFT JOIN u.usersIFollow uif "
 			+ "WHERE uif.id = :otherUserId AND u.id = :userId")
 	public UserModel findUserIFollowFromUser(@Param("otherUserId") int recId, @Param("userId") int userId);
 	
 	// give me the user with the username with its allergies and lovedIngredients and hatedIngredients
 	@Query ("SELECT u "
 			+ "FROM UserModel AS u "
-			+ "JOIN FETCH u.allergies a "
-			+ "JOIN FETCH u.lovedIngredients li "
-			+ "JOIN FETCH u.hatedIngredients hi "
+			+ "LEFT JOIN FETCH u.allergies a "
+			+ "LEFT JOIN FETCH u.lovedIngredients li "
+			+ "LEFT JOIN FETCH u.hatedIngredients hi "
 			+ "WHERE LOWER(u.userName) = LOWER(:userName) ")
 	public UserModel findUserByUserNameWithAllergiesAndLovedIngredientsAndHatedIngredients(@Param("userName") String userName);
 	
 	// give me the user with the username with its allergies
 	@Query ("SELECT u "
 			+ "FROM UserModel AS u "
-			+ "JOIN FETCH u.allergies a "
+			+ "LEFT JOIN FETCH u.allergies a "
 			+ "WHERE LOWER(u.userName) = LOWER(:userName) ")
 	public UserModel findUserByUserNameWithAllergies(@Param("userName") String userName);
 	
 	// give me the user with the username with its lovedIngredients
 	@Query ("SELECT u "
 			+ "FROM UserModel AS u "
-			+ "JOIN FETCH u.lovedIngredients li "
+			+ "LEFT JOIN FETCH u.lovedIngredients li "
 			+ "WHERE LOWER(u.userName) = LOWER(:userName) ")
 	public UserModel findUserByUserNameWithLovedIngredients(@Param("userName") String userName);
 	
 	// give me the user with the username with its hatedIngredients
 	@Query ("SELECT u "
 			+ "FROM UserModel AS u "
-			+ "JOIN FETCH u.hatedIngredients hi "
+			+ "LEFT JOIN FETCH u.hatedIngredients hi "
 			+ "WHERE LOWER(u.userName) = LOWER(:userName) ")
 	public UserModel findUserByUserNameWithHatedIngredients(@Param("userName") String userName);
 		
